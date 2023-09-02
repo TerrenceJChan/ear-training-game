7ef36f9ff09e60b65b37ac45ef09196148e24b33
Terrence Chan <terrencechan.writer@gmail.com>
2023-09-02 13:54:49 -0400

    Initialize front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-300">
-            <p>Hello World</p>
+        <main className="min-h-screen flex flex-row p-24 gap-4 bg-gray-300">
+            <div className="flex flex-1 bg-red-500 flex-col justify-center items-center">
+                <div className="flex flex-col gap-8">
+                    <button className="p-4 border border-1">Play Game</button>
+                    <button className="p-4 border border-1">Points Shop</button>
+                </div>
+            </div>
+            <div className="flex flex-1 flex-col justify-center items-center bg-red-500">
+                <div className="grid grid-cols-2 gap-x-4 gap-y-6 text-left"></div>
+            </div>
         </main>
     )
 }
