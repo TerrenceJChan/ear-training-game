ee09e97a90abb21b5bb146fa440a8ea78b727c16
Terrence Chan <terrencechan.writer@gmail.com>
2023-09-04 15:55:48 -0400

    Set up recoil

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 
+import { useRecoilValue } from "recoil"
+
+import { maxNoteState } from "@/atoms/upperBoundState"
 import chooseTwoNotes from "@/utils/chooseTwoNotes"
 
 export default function Game() {
+    // const [upperBound, setUpperBound] = useRecoilState(upperBoundState)
+    const maxNote = useRecoilValue(maxNoteState)
     return (
         <main className="min-h-screen flex p-24 gap-4 bg-gray-300">
             <button onClick={() => chooseTwoNotes(30, 40, "up")}>
-                Play Note
+                Upper Bound = {maxNote}
             </button>
         </main>
     )
